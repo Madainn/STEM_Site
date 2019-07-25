dcabf56d1f91569abe1232b434b71663f8611a88
Madainn <madainn.krall.17@tfacorps.org>
2019-07-24 20:55:43 -0400

    Added HOF

---
+++ hallOfFame.js
@@ -0,0 +1,3 @@
+const signupContainer = document.querySelector('#signupContainer')
+const closeSignup = document.querySelector('#closeSignup')
+closeSignup.addEventListener('click', () => signupContainer.style.display = 'none')
