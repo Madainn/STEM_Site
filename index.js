00f0bff63af4f9fbd40248de5686c6b89816c402
Madainn <madainn.krall.17@tfacorps.org>
2019-08-31 10:30:31 -0400

    alot added

--- index.js
+++ index.js
@@ -1,1 +1,1 @@
-alert("HI! Thank you for visiting! Many parts of this website are still VERY much under construction! Check back regularly if you're looking forward to a specific part of the site!")
+// alert("HI! Thank you for visiting! Many parts of this website are still VERY much under construction! Check back regularly if you're looking forward to a specific part of the site!")
