f983613a6f55394848d122b04f0eb6dce0596209
Madainn <madainn.krall.17@tfacorps.org>
2020-03-13 09:54:09 -0400

    Popup on homepage

--- index.js
+++ index.js
@@ -1,1 +1,4 @@
 // alert("HI! Thank you for visiting! Many parts of this website are still VERY much under construction! Check back regularly if you're looking forward to a specific part of the site!")
+const signupContainer = document.querySelector('#signupContainer')
+const closeSignup = document.querySelector('#closeSignup')
+closeSignup.addEventListener('click', () => signupContainer.style.display = 'none')
